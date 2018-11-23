a1e304923dad7aa2bb308e30611a6acac9c7d744
Kodai Suemitsu <e185701@ie.u-ryukyu.ac.jp>
2018-11-23 22:38:24 +0900

    step2

--- src/Exception.java
+++ src/Exception.java
@@ -1,6 +1,14 @@
+import java.io.IOException;
+
 public class Exception {
-    public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+    public static void main(String[] args) {
+        try {
+            String str = null;
+
+            System.out.println(str.length());
+        }catch (NullPointerException e){
+            System.out.println("NullPointerExceptionが発生");
+            System.out.println(e.getMessage());
+        }
     }
 }
